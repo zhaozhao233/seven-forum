38e05dedc45bc7e97e567ed695a9bf504cf289d4
riven <1595606639@qq.com>
2020-04-01 13:12:00 +0800

    dynamic and comment

--- seven-forum-controller/src/main/java/com/seven/forum/controller/lhj/UserDynamicController.java
+++ seven-forum-controller/src/main/java/com/seven/forum/controller/lhj/UserDynamicController.java
@@ -6,21 +6,35 @@ import com.seven.forum.service.lhj.UserDynamicService;
 import com.seven.forum.vo.ResponseVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 @Controller
-@ResponseBody
 @RequestMapping("/dynamic")
+@CrossOrigin(value = "http://127.0.0.1:8848",
+                methods = {RequestMethod.DELETE,RequestMethod.GET,RequestMethod.POST},
+            allowCredentials = "true")
 public class UserDynamicController {
+
+    private static final String FILE_DIRECTORY = "D:\\A-learn\\A-TeamProject\\seven-forum\\seven-forum-controller\\src\\main\\resources\\static\\image";
+
+
     @Autowired
     private UserDynamicService dynamicService;
 
-    //显示我关注的人发的动态
+    //显示我and关注的人发的动态
     @RequestMapping("/followUserDynamic")
+    @ResponseBody
     public ResponseVO listDynamic(Integer pageNum,Integer pageSize,Integer userId){
         List<UserDynamicEntity> userDynamicEntities = dynamicService.listFollowUserDynamic(pageNum, pageSize, userId);
         ResponseVO vo = new ResponseVO();
@@ -53,10 +67,25 @@ public class UserDynamicController {
         return "reply done";
     }
 
-    @RequestMapping("/releaseDynamic")
+    @PostMapping("/releaseDynamic")
     @ResponseBody
     //发布动态
-    public String releaseDynamic(Integer userId,String dynamicContent){
+    public String releaseDynamic(Integer userId, String dynamicContent, MultipartFile[] myFiles){
+        String filename;
+        String newFilename;
+        String ext;
+        for (MultipartFile myFile : myFiles) {
+             filename =  myFile.getOriginalFilename();
+             newFilename = UUID.randomUUID().toString();
+             ext = filename.substring(filename.lastIndexOf("."));
+            dynamicContent+="<img src='"+FILE_DIRECTORY+newFilename+ext+"'/>";
+            Path path = FileSystems.getDefault().getPath(FILE_DIRECTORY,newFilename + ext);
+            try {
+                myFile.transferTo(path);
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
         dynamicService.releaseDynamic(userId,dynamicContent);
         return "release done";
     }
@@ -67,19 +96,16 @@ public class UserDynamicController {
     public String likeDynamic(Integer likeObjId, Integer userId, @RequestParam(defaultValue = "1") Integer likeStatus, Integer dynamicId){
         Integer status = dynamicService.checkDynamicLike(likeObjId, userId);
         if (status==null){
-            System.out.println("添加记录-----");
             dynamicService.likeDynamicAndAddLikeCount(likeObjId,userId,likeStatus,dynamicId);
             return "insert done";
         }else {
             //取消点赞
             if (status==1){
-                System.out.println("取消-----");
                 dynamicService.cancelDynamicLikeAndReduceLikeCount(likeObjId,userId,dynamicId);
                 return "cancel";
             }
             //取消点赞后又点赞
             if (status==0){
-                System.out.println("取消后------");
                 dynamicService.dynamicLikeAgainAfterCancelLikeAndAddLikeCount(likeObjId,userId,dynamicId);
                 return "after cancel";
             }
@@ -94,7 +120,7 @@ public class UserDynamicController {
     public String likeComment(Integer likeObjId, Integer userId, @RequestParam(defaultValue = "1") Integer likeStatus, Integer commentId){
         Integer status = dynamicService.checkCommentLike(likeObjId, userId);
         if (status==null){
-            System.out.println("添加记录-----");
+            //点赞
             dynamicService.likeCommentAndAddLikeCount(likeObjId,userId,likeStatus,commentId);
             return "insert done";
         }else {
