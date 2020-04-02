c852b52ff3e06d12a75f836ffaec94d9deae4b57
xie min <1677601365@qq.com>
2020-04-02 21:53:20 +0800

    使用无参构造注解

--- seven-forum-controller/src/main/java/com/seven/forum/vo/ResponseVO.java
+++ seven-forum-controller/src/main/java/com/seven/forum/vo/ResponseVO.java
@@ -2,41 +2,15 @@ package com.seven.forum.vo;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ResponseVO<T> {
 
     private Integer code;
     private String message;
     private T data;
 
-    // 一定要有构造函数，就算加了@Data注解也很有可能坑si自己
-    public ResponseVO() {
-
-    }
-
-    public Integer getCode() {
-        return code;
-    }
-
-    public void setCode(Integer code) {
-        this.code = code;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public T getData() {
-        return data;
-    }
-
-    public void setData(T data) {
-        this.data = data;
-    }
 }
