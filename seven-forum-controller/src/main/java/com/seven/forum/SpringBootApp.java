dff9f27ad85b2da66d5539ead409a754b28204f7
riven <1595606639@qq.com>
2020-04-03 14:46:17 +0800

    dynamic

--- seven-forum-controller/src/main/java/com/seven/forum/SpringBootApp.java
+++ seven-forum-controller/src/main/java/com/seven/forum/SpringBootApp.java
@@ -1,9 +1,9 @@
 package com.seven.forum;
 
 import com.seven.forum.entity.lhj.UserDynamicEntity;
-import com.seven.forum.entity.zyl.PostBarPartitionEntity;
+
 import com.seven.forum.service.lhj.UserDynamicService;
-import com.seven.forum.service.zyl.PostBarPartitionService;
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,11 +18,7 @@ public class SpringBootApp {
     public static void main(String[] args) {
         ConfigurableApplicationContext run =
                 SpringApplication.run(SpringBootApp.class, args);
-//        PostBarPartitionService bean = run.getBean(PostBarPartitionService.class, run);
-//        List<PostBarPartitionEntity> postBarPartitionEntities = bean.listAllPartitionsAndCatalogues();
-//        for (PostBarPartitionEntity postBarPartitionEntity : postBarPartitionEntities) {
-//            System.out.println(postBarPartitionEntity);
-//        }
+
         UserDynamicService bean = run.getBean(UserDynamicService.class, run);
         List<UserDynamicEntity> userDynamicEntities = bean.listFollowUserDynamic(1,2,123);
         for (UserDynamicEntity userDynamicEntity : userDynamicEntities) {
--- seven-forum-controller/src/main/java/com/seven/forum/vo/ResponseVO.java
+++ seven-forum-controller/src/main/java/com/seven/forum/vo/ResponseVO.java
@@ -16,6 +16,7 @@ public class ResponseVO<T> {
 
     }
 
+
     public Integer getCode() {
         return code;
     }
